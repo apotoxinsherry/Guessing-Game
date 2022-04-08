6f598a7d9efdf6be8d36f1054cb3158b02dff18b
apotoxinsherry <anirudhganapathy2003@gmail.com>
2022-04-08 22:08:23 +0530

    Implemented the guessing game logic.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,26 @@
 files=$(ls | wc -l)
-read input
+
+function guess {
+    while [[ 1 -gt 0 ]]
+    do
+        read input
+        if [[ $input -eq $files ]]
+        then
+            echo "Congratulations, you got it correct"
+            break
+        elif [[ $input -lt $files ]]
+        then
+            echo "Your guess is less than the actual number of files, try again"
+            continue
+        elif [[ $input -gt $files ]]
+        then
+            echo "Your guess is higher than the actual number of files, try again"
+            continue
+        else   
+            continue
+        fi
+    done
+
+}
+
+guess
